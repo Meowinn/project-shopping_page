6ff24dca2abdd4b1d65a97354705272daccdd01e
equinox <darwinfagarang@gmail.com>
2023-03-01 11:41:01 +0800

    add basename

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ import Shop from './components/shop';
 const App = () => {
   return (
     <div id="App">
-      <BrowserRouter>
+      <BrowserRouter basename='/home-page'>
       <Routes>
-        <Route path='/' element={<Home />}></Route>
+        <Route path='/home-page' element={<Home />}></Route>
         <Route path="/shop" element={<Shop />}></Route>
       </Routes>
       </BrowserRouter>
